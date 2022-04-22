8e0f92cc568f19010cd64ffd034b9e23a7617a80
Tyler J. Gamlem <tgamlem22@my.whitworth.edu>
2022-04-22 14:29:54 -0700

    comments and cleaning up

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,7 @@
+// Tyler Gamlem, Ezekiel Pierson
+// CS 475W - Lab 6
+// April 22, 2022
+
 #include <iostream>
 #include <map>
 
@@ -29,11 +33,16 @@ void invaliddelete_2() {
     //invalid delete
     delete p;
 }
+
+// flags are set to ensure that only new and delete calls from the program are tracked
 bool track_new = false;
 bool track_delete = false;
+// map to track what pointers have been allocated
 map<size_t, int> pointerTrackerState;
+// map to track the size of pointer allocations when using new[]
 map<size_t, size_t> pointerSizeTracker;
 
+// reset the flags and maps
 void resetState() {
     #ifndef DEBUG_MEMORY
         cout << "This is debug code only " << endl;
@@ -44,6 +53,7 @@ void resetState() {
     #endif
 }
 
+// use the results at the end of a function to determine if errors exist
 void checkForErrors() {
     #ifndef DEBUG_MEMORY
         cout << "This is debug code only " << endl;
@@ -66,14 +76,17 @@ int main() {
     resetState();
     invaliddelete_1();
     checkForErrors();
-    resetState();
+
     cout << "Start invaliddelete_2()" << endl;
+    resetState();
     invaliddelete_2();
     checkForErrors();
-    resetState();
+
     cout << "Start memoryLeak()" << endl;
+    resetState();
     memoryleak();
     checkForErrors();
+
     cout << "Start bufferoverrun()" << endl;
     resetState();
     bufferoverrun();
@@ -81,7 +94,7 @@ int main() {
 }
  
 // overload global new and global delete
-void * operator new(std::size_t n) //throw(std::bad_alloc)
+void * operator new(std::size_t n)
 {
     #ifndef DEBUG_MEMORY
     void *new_ptr = malloc(n);
@@ -91,7 +104,7 @@ void * operator new(std::size_t n) //throw(std::bad_alloc)
     if(track_new) {
         track_new = false;
         if (pointerTrackerState.count(n) > 0) {
-            cout << "Error ? " << n << endl;
+            cout << "Error allocating " << n << endl;
         } else {
             pointerTrackerState.insert(pair<size_t, int>(size_t(new_ptr), 1));
         }
@@ -105,16 +118,18 @@ void operator delete(void * p) throw()
     #ifndef DEBUG_MEMORY
     free(p);
 #else
+    // if the pointer is being tracked
     if (pointerTrackerState.count(size_t(p)) > 0) {
         pointerTrackerState[size_t(p)] = pointerTrackerState[size_t(p)] - 1;
         if (pointerTrackerState[size_t(p)] == 0) {
             free(p);
         }
-        
+    // if this would be an invalid delete
     } else if (track_delete) {
         track_new = false;
         pointerTrackerState.insert(pair<size_t, int>(size_t(p), -1));
         track_delete = false;
+    // allow non-tracked memory to be deleted by the system
     } else {
         free(p);
     }
@@ -126,15 +141,18 @@ void * operator new[](size_t sz) {
 #ifndef DEBUG_MEMORY
     return malloc(sz)
 #else
+    // if we want to track this pointer
     if(track_new) {
         char *m = (char *)malloc(sz + 8);
         track_new = false;
         if (pointerTrackerState.count(sz) > 0) {
-            cout << "Error ? " << sz << endl;
+            cout << "Error " << sz << endl;
         } else {
             pointerTrackerState.insert(pair<size_t, int>(size_t(m), 1));
         }
+        // track size for new[] calls
         pointerSizeTracker.insert(pair<size_t, size_t>(size_t(m), sz));
+        // set the allocated bits that border the memory to 0
         for (int i = 0; i < 4; i++) {
             *m = '0';
             m = m++;
@@ -143,6 +161,7 @@ void * operator new[](size_t sz) {
             *(m+i) = '0';
         }
         return m;
+    // let system calls still allocate normally
     } else {
         return  malloc(sz);
     }
@@ -155,6 +174,7 @@ void operator delete[] (void * p) noexcept {
     if (track_delete) {
         char * ptr = (char *) p;
         ptr = ptr--;
+        // check if the bits bordering the pointer are still 0, if not then buffer overrun
         for (int i = 0; i < 4; i++) {
             if (*ptr != '0') {
                 cout << "Buffer Overrun detected!" << endl;
@@ -168,16 +188,18 @@ void operator delete[] (void * p) noexcept {
             }
         }
     }
+    // free valid deletes for tracked pointers
     if (pointerTrackerState.count(size_t(p)) > 0) {
         pointerTrackerState[size_t(p)] = pointerTrackerState[size_t(p)] - 1;
         if (pointerTrackerState[size_t(p)] == 0) {
             free(p);
         }
-        
+    // these deletes are invalid
     } else if (track_delete) {
         track_new = false;
         pointerTrackerState.insert(pair<size_t, int>(size_t(p), -1));
         track_delete = false;
+    // let the system free its pointers
     } else {
         free(p);
     }
